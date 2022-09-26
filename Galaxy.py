8fc578196a441bb110a903be677bc44a5b0cc3c0
Aleksandr <45598303+Horoshenkiyempty@users.noreply.github.com>
2022-09-26 08:18:00 +0300

    main program

--- Galaxy.py
+++ Galaxy.py
@@ -11,7 +11,7 @@ ALPHA = 120
 class Star:
         def __init__(self, app):
             self.screen = app.screen
-            self.pos3d = sel.get_pos3d
+            self.pos3d = self.get_pos3d
             self.vel = random.uniform(0.05, 0.25)
             self.color = random.choice(COLORS)
             self.screen_pos = vec2(0,0)
@@ -27,16 +27,17 @@ class Star:
             self.pos3d = self.get_pos3d() if self.pos3d.z < 1 else self.pos3d
             self.screen_pos = vec2 (self.pos3d.x, self.pos3d.y) / self.pos3d.z + CENTER
             self.size = (Z_DISTANCE - self.pos3d.z) / (0.2 * self.pos3d.z)
+            self.pos3d.xy = self.pos3d.xy.rotate(0.2)
         def update(self):
             s = self.size
-            if(-s < self.screen_pos.x < WIDTH + s) and (-s < self.screen_pos.y < HEIGHT + s)
+            if(-s < self.screen_pos.x < WIDTH + s) and (-s < self.screen_pos.y < HEIGHT + s):
                 pg.draw.rect(self.screen, self.color, (*self.screen_pos, self.size, self.size))
 class Starfield:
     def __init__(self, app):
         self.stars = [Star(app) for i in range (NUM_STARS)]
     def run(self):
         [star.update() for star in self.stars]
-        self.stars.sort(key=lambda star: star.pos3d.z reverse=True)
+        self.stars.sort(key=lambda star: star.pos3d.z, reverse=True)
         [star.draw() for star in self.stars]
 class App:
     def __init__(self):
@@ -45,7 +46,7 @@ class App:
         self.alpha_surface.set.alpha(ALPHA)
         self.clock = pg.time.Clock()
         self.starfield = Starfield(self)
-        def run(self)
+        def run(self):
             while True:
                 self.screen.blit(self.alpha_surface, (0, 0))
                 self.starfield.run()
